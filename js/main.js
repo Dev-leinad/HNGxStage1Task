0f5ac63b55e5a2946e600b2bb1567d3f9bd2d8f6
Dev-leinad <ochidaniel075@gmail.com>
2023-09-09 19:08:32 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+// Gets the current day 
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let displayDay = document.getElementById('currDay');
+displayDay.innerHTML = days[new Date().getDay()];
+
+// Gets the current  UTC
+
+let displayUtc = document.getElementById('utc');
+displayUtc.innerHTML = new Date (24*60*60*1000);
